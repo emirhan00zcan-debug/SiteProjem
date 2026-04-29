6141f5e436dcf7839f1f1f72b5e2624031f8b807
emirhan00zcan-debug <emirhan00zcan@gmail.com>
2026-04-29 14:59:21 +0300

    tamam

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import ScrollReveal from '@/components/ScrollReveal'
+import Script from 'next/script' // Import en yukarıda olmalı
 
 export const metadata: Metadata = {
     title: 'Özcan Mobilya',
@@ -22,6 +23,22 @@ export default function RootLayout({
                     rel="stylesheet"
                 />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+                {/* Facebook Pixel Kodu Head İçinde Olmalı */}
+                <Script id="fb-pixel" strategy="afterInteractive">
+                    {`
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '1277633564560946');
+                        fbq('track', 'PageView');
+                    `}
+                </Script>
             </head>
             <body>
                 <ScrollReveal />
